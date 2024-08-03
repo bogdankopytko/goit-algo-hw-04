a3cf5d991b345f8966afb3cf76cfaeb214b0e9af
bogdankopytko <bo.kopytko@gmail.com>
2024-08-03 14:56:25 +0200

    Changed the function call.

--- task_2/index.py
+++ task_2/index.py
@@ -1,11 +1,9 @@
-from pathlib import Path
-
-INFO_CATS_FILE = Path('cats_information.txt')
+INFO_CATS_FILE = 'cats_information.txt'
 
 
 def get_cats_info(path):
     list_of_cats = []
-    if path.exists():
+    try:
         with open(path, 'r', encoding='utf-8') as file:
             for line in file:
                 cat_id, cat_name, cat_age = line.split(',')
@@ -13,8 +11,9 @@ def get_cats_info(path):
                 cat = {'id': cat_id, 'name': cat_name, 'age': cat_age}
                 list_of_cats.append(cat)
         return list_of_cats
-    else:
-        print(f'File {path} not exist!')
+    except FileNotFoundError:
+        return print(f'File {path} does not exist!')
 
 
-print(get_cats_info(INFO_CATS_FILE))
+if get_cats_info(INFO_CATS_FILE):
+    print(get_cats_info(INFO_CATS_FILE))
