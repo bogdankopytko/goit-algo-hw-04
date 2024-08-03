8620420138a8648b00641eff6d5e54aba04b1e07
bogdankopytko <bo.kopytko@gmail.com>
2024-08-03 13:11:20 +0200

    Changed the type of input and output of the function, changed type of salary from int to float.

--- task_1/index.py
+++ task_1/index.py
@@ -1,23 +1,21 @@
-from pathlib import Path
-
-SALARY_FILE = Path('salary.txt')
+SALARY_FILE = 'salary.txt'
 
 
 def total_salary(path):
     sum_salary = 0
     quantity_of_employee = 0
-    if path.exists():
+    try:
         with open(path) as file:
             for line in file:
                 employee_name, salary = line.split(',', maxsplit=1)
                 salary = salary.rstrip('\n')
-                sum_salary += int(salary)
+                sum_salary += float(salary)
                 quantity_of_employee += 1
-    else:
-        return print(f'File {path} not exist!')
+    except FileNotFoundError:
+        return print(f'File {path} does not exist!')
     average_salary = sum_salary / quantity_of_employee
-    result = (sum_salary, average_salary)
-    print(f'Summary salary = {result[0]}, average salary = {result[1]}')
+    return sum_salary, average_salary
 
 
-total_salary(SALARY_FILE)
+total, average = total_salary(SALARY_FILE)
+print(f'Summary salary = {total}, average salary = {average}')
